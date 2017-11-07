d05b6545e9a5b1545fde9274eb339f98f8f93e12
heroku <joseluisquichimbo91@gmail.com>
2017-11-07 01:15:15 -0500

    Agregado variable settings STATIC_ROOT

--- BootateTV/settings.py
+++ BootateTV/settings.py
@@ -71,7 +71,7 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    
+
 ]
 
 ROOT_URLCONF = 'BootateTV.urls'
@@ -149,7 +149,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS=(os.path.join(BASE_DIR, 'static'),)
